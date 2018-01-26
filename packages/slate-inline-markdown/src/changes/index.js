9defbf548064af21bcfd319c75086860d60aa890
Ben <menubar@protonmail.com>
2018-01-26 17:59:44 -0500

    new

--- packages/slate-inline-markdown/src/changes/index.js
+++
@@ -1,7 +0,0 @@
-import insertMark from './insertMark'
-import insertSpace from './insertSpace'
-import replaceText from './replaceText'
-
-exports.insertMark = insertMark;
-exports.replaceText = replaceText;
-exports.insertSpace = insertSpace;
