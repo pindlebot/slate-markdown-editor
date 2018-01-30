2fd5db01ceadbb5f84597d6a6f648084f26c440e
Ben <menubar@protonmail.com>
2018-01-30 00:36:42 -0500

    fix(pencil): update

--- bin/deploy-example.sh
+++ bin/deploy-example.sh
@@ -3,7 +3,7 @@ cd example
 rm -rf dist
 rm -rf .cache
 NODE_ENV=production parcel build src/index.html --no-minify --public-url / --no-cache
-yarn add slate-markdown-editor@latest
+yarn add slate-markdown-editor@latest --ignore-engines
 git init
 git add .
 git commit -am 'new'
