79e0a22ba4f9f1a353ba2298fa8343ac9e3b3d2e
Ben <menubar@protonmail.com>
2018-01-29 21:41:02 -0500

    new

--- bin/prepublish.sh
+++ bin/prepublish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-git add . 
-git commit -am 'new'
-npm version patch
+# git add . 
+# git commit -am 'new'
+# npm version patch
 babel src/ -d lib/ --ignore src/plugins/packages/
 echo 'export default []' > lib/plugins/dev.js
+# npm publish
