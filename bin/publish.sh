c6922e5c2f4e1f0558c696ed8b402b9424842897
Ben <menubar@protonmail.com>
2018-01-30 00:29:24 -0500

    fix(pencil): update

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git add . 
 git commit -am 'fix(pencil): update'
+git push origin -u master
 babel src/ -d lib/ --ignore src/plugins/packages/
 echo 'export default []' > lib/plugins/dev.js
 export $(cat .env | xargs) && semantic-release --debug --no-ci
+sh deploy-example.sh
