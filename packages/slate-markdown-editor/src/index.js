775fef64d4d406aa27b41e748d1ec3f674990a64
Ben <menubar@protonmail.com>
2018-02-13 06:59:24 -0500

    new

--- packages/slate-markdown-editor/src/index.js
+++ packages/slate-markdown-editor/src/index.js
@@ -4,7 +4,7 @@ import Editor from './Editor'
 import defaultProps from './props'
 import * as util from './plugins/main/helpers'
 
-exports.default = Editor
+exports.default = Editor()
 exports.Editor = Editor
 exports.defaultProps = defaultProps
 exports.util = util
